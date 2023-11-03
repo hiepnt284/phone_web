86c091137a411e5855743df048df3dd85566c1af
hiepnt284 <hiepyolo02@gmail.com>
2023-11-03 22:40:39 +0700

    update

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,16 @@
 package model;
 
 public class User {
-
+	private String username;
+	private String phone;
+	private String password;
+	private String address;
+	
+	public User(String username, String phone, String password) {
+		this.username = username;
+		this.phone = phone;
+		this.password = password;
+	}
+	
+	
 }
